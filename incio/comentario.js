8868885fe51488995418e9b23565e3bc7a90c724
Blouck <joaovictor121999@gmail.com>
2024-07-28 14:12:51 -0300

    first commit

---
+++ incio/comentario.js
@@ -0,0 +1,23 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log('Passo #01');
+console.log('Passo #02'); // mais um exemplo de console.log!
+// console.log('Passo #03'); colocando as duas barras no inicio da sentença não sera executada
+
+/*
+ * Esse é um
+ * comentario
+ * de multiplas
+ * linhas 
+*/
+
+// bloco de codigos é definido por um par de { };
+
+// como um codigo de programa é organizado?
+// normalmente é organaizado em pastas, em arquivos de determinados tipos de programa: exemplo: .js, .py, .c
+// atalho para duplicar linha "Alt + Shift + seta pra baixo"
+
+console.log(10); // valor literal: quando você coloca um valor direto
+console.log(6.4); // para separar as casas decimais usamos "." e não ","
+var quantidade = 10; // o sintal de "=" é uma atribuição, ou seja na variavel quantidade
+// o valor atribuido foi 10
+// var ou let ambas criam variaveis com algumas diferenças
